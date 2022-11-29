3687fa0493820cfbcc7c199a29e5321df3d952da
xqnode <xqnode@163.com>
2022-11-29 21:51:57 +0800

    2. 注册和Mybatis-Plus

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,8 +1,8 @@
 <template>
-  <div style="height: 100vh;overflow: hidden; position: relative; background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);">
+  <div style="height: 100vh;overflow: hidden; position: relative; background-image: linear-gradient(to top, #accbee 0%, #e7f0fd 100%);">
     <div class="form-box">
       <el-form ref="ruleFormRef" :model="form" :rules="rules" status-icon>
-        <h2 style="text-align: center; color: dodgerblue">登 录</h2>
+        <h2 style="text-align: center; color: dodgerblue">注 册</h2>
         <el-form-item prop="username">
           <el-input v-model="form.username" placeholder="请输入账号" :prefix-icon="User"></el-input>
         </el-form-item>
@@ -10,11 +10,18 @@
           <el-input v-model="form.password" show-password placeholder="请输入密码"
                     autocomplete="new-password" :prefix-icon="Lock"></el-input>
         </el-form-item>
+        <el-form-item prop="confirm">
+          <el-input v-model="form.confirm" show-password placeholder="请确认密码"
+                    autocomplete="new-password" :prefix-icon="Lock"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.name" placeholder="请设置昵称" :prefix-icon="UserFilled"></el-input>
+        </el-form-item>
         <div style="margin-bottom: 0.83em">
-          <el-button style="width: 100%" type="primary" @click="login">登录</el-button>
+          <el-button style="width: 100%" type="primary" @click="register">注册</el-button>
         </div>
         <div style="text-align: right">
-          <el-button type="primary" link @click="router.push('/register')">没有账号？请注册</el-button>
+          <el-button type="primary" link @click="router.push('/login')">已有账号？请登录</el-button>
         </div>
       </el-form>
     </div>
@@ -24,13 +31,26 @@
 <script setup>
 import {reactive, ref} from "vue"
 // import { FormInstance, FormRules } from 'element-plus'
-import { User, Lock } from '@element-plus/icons-vue'
+import { User, Lock, UserFilled } from '@element-plus/icons-vue'
 import router from "@/router";
 import request from "@/utils/request";
 import {ElMessage} from "element-plus";
 import { useUserStore } from "@/stores/user";
 
 const ruleFormRef = ref()
+const form = reactive({})
+const store = useUserStore()
+
+
+const confirmPassword = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error('请确认密码'))
+  }
+  if (form.password !== value) {
+    callback(new Error('两次输入密码不一致'))
+  }
+  callback()
+}
 const rules = reactive({
   username: [
     { required: true, message: '请输入账号', trigger: 'blur'},
@@ -38,21 +58,19 @@ const rules = reactive({
   password: [
     { required: true, message: '请输入密码', trigger: 'blur'},
   ],
+  confirm: [
+    { validator: confirmPassword, trigger: 'blur'},
+  ],
 })
 
-const form = reactive({})
-const store = useUserStore()
-const login = () => {
-  console.log(ruleFormRef)
+
+const register = () => {
   ruleFormRef.value.validate(valid => {
-    // 当 valid  === true  就可以调用登录接口了
     if (valid) {
-      request.post("/login", form).then(res => {
-        console.log(res)
+      request.post("/register", form).then(res => {
         if (res.code === '200') {
-         //   store.$patch({user: res.data})   // res.data 是后台返回的用户数据，存储到缓存里面
           store.setUser(res.data)
-          ElMessage.success('登录成功')
+          ElMessage.success('注册成功')
           router.push('/')
         } else {
           ElMessage.error(res.msg)
@@ -72,7 +90,7 @@ console.log(store.user)
   border-radius: 10px;
   margin: 0 auto;
   box-shadow: 0 0 5px -2px rgba(0, 0, 0, .5);
-  background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
+  background-image: linear-gradient(120deg, #e0c3fc 0%, #8ec5fc 100%);
   padding: 20px;
   position: absolute;
   top: 50%;
